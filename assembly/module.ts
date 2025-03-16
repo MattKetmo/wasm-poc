5c3e8a65e3d34cd8d377d71fc5fa6694e05c3a62
Matthieu Moquet <matthieu@moquet.net>
2025-03-16 23:23:24 +0100

    clean unused

--- assembly/module.ts
+++ assembly/module.ts
@@ -1,6 +1,5 @@
 import { Point } from "./types";
 
-// @entrypoint
 export function score(point: Point): i32 {
   return i32(point.x) + i32(point.y);
 }
--- transform/src/index.ts
+++ transform/src/index.ts
@@ -1,13 +1,5 @@
 import { SimpleParser, TransformVisitor } from "visitor-as/dist/index.js";
-import {
-  Expression,
-  CallExpression,
-  IdentifierExpression,
-  Parser,
-  Source,
-  NodeKind,
-  FunctionDeclaration,
-} from "assemblyscript/dist/assemblyscript.js";
+import { Parser } from "assemblyscript/dist/assemblyscript.js";
 
 const entrypointFunctionText = `
 export function entrypoint(): i32 {
@@ -31,38 +23,6 @@ class EntrypointTransformer extends TransformVisitor {
 
       this.visit(source);
     });
-  }
-
-  override visitFunctionDeclaration(
-    node: FunctionDeclaration,
-    isDefault = false
-  ): FunctionDeclaration {
-    // const name = node.name.text;
-    // if (name !== "score") {
-    //   return super.visitFunctionDeclaration(node, isDefault);
-    // }
-    return super.visitFunctionDeclaration(node, isDefault);
-
-    console.log("visitFunctionDeclaration", node.name.text);
-
-    // Create an entrypoint function that calls score with a new Point(1,2)
-    const entrypointFunctionText = `
-export function entrypoint(): i32 {
-  // return score(new Point(1, 2));
-  return 42;
-}`;
-
-    // Parse the entrypoint function text into an AST node
-    const entrypointFunction = SimpleParser.parseStatement(
-      entrypointFunctionText
-    );
-
-    // Add the entrypoint function to the source file
-    if (node.range && node.range.source && node.range.source.statements) {
-      node.range.source.statements.push(entrypointFunction);
-    }
-
-    return super.visitFunctionDeclaration(node, isDefault);
   }
 }
 
