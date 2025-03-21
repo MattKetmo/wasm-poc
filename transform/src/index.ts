91f848bee3615c17dd078daf9fe8c77aef67a5c7
Matthieu Moquet <matthieu@moquet.net>
2025-03-21 13:04:41 +0100

    test without visitor-as

--- transform/src/index.ts
+++ transform/src/index.ts
@@ -1,29 +1,31 @@
-import { SimpleParser, TransformVisitor } from "visitor-as/dist/index.js";
-import { Parser } from "assemblyscript/dist/assemblyscript.js";
+import { Parser, Tokenizer, Source, SourceKind } from "assemblyscript/dist/assemblyscript.js";
+import { Transform } from "assemblyscript/dist/transform.js";
 
-const entrypointFunctionText = `
-export function entrypoint(): i32 {
-  return score([new Point(1, 2), new Point(3, 4)]);
-}
-`;
-
-class EntrypointTransformer extends TransformVisitor {
+export default class InjectEntrypointTransform extends Transform {
   afterParse(parser: Parser): void {
     const sources = parser.sources;
 
-    sources.forEach((source) => {
-      if (!source.text) return;
-
+    for (const source of sources) {
       if (source.simplePath === "module") {
-        const entrypointFunction = SimpleParser.parseTopLevelStatement(
-          entrypointFunctionText
-        );
-        source.statements.push(entrypointFunction);
-      }
 
-      this.visit(source);
-    });
+        const functionToInject = `
+export function entrypoint(): i32 {
+  const points = [new Point(1, 2), new Point(3, 4)];
+  return score(points);
+}`;
+
+        const injectSource = new Source(
+          SourceKind.User, // sourceKind (USER for user-provided code)
+          "inject.ts",     // normalizedPath
+          functionToInject // text content
+        );
+
+        const tokenizer = new Tokenizer(injectSource);
+        const injectedStatements = parser.parseTopLevelStatement(tokenizer)
+        source.statements.push(injectedStatements);
+
+        console.log(`Injected 'entrypoint' function into '${source.simplePath}'`);
+      }
+    }
   }
 }
-
-export default EntrypointTransformer;
